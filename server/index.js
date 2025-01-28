8941397f7dd5875e0dddcd3473877d8d39cc2744
Ali Gaineshev <ggaineshev@gmail.com>
2025-01-28 11:53:37 -0800

    add cors for prod, configure custom headers to test in dev

--- server/index.js
+++ server/index.js
@@ -9,7 +9,30 @@ const connectDb = require('./src/services/db-connection.js')
 connectDb();
 /* Middleware to parse JSON and URL-encoded data */
 const cors = require('cors');
-app.use(cors());
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+if (process.env.NODE_ENV !== 'PROD') {
+    // Disable CORS in dev (proxy handles it)
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(204);
+        }
+        next();
+    });
+} else {
+    // Use CORS in prrod
+    app.use(
+        cors({
+            origin: process.env.CLIENT_URL, // Allow requests from frontend
+            credentials: true, // Allow cookies and other credentials
+        })
+    );
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 /* Routes */
