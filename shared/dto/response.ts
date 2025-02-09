ce77358adaf2b00192a4ac82df40d506ea184eba
Ali Gaineshev <ggaineshev@gmail.com>
2025-02-08 22:45:22 -0800

    add toJson() and change the constrcutor

--- shared/dto/response.ts
+++ shared/dto/response.ts
@@ -1,10 +1,16 @@
 export class ResponseDTO<T> {
-    constructor(
-        private readonly success: boolean,
-        private readonly data?: T,
-        private readonly message?: string,
-        private readonly error?: string,
-    ) {}
+
+    private readonly success: boolean;
+    private readonly data?: T;
+    private readonly message?: string;
+    private readonly error?: any;
+
+    constructor({ success, data, message, error }: { success: boolean; data?: T; message?: string; error?: any }) {
+        this.success = success;
+        this.data = data;
+        this.message = message;
+        this.error = error;
+    }
 
     isSuccess(): boolean {
         return this.success;
@@ -20,6 +26,15 @@ export class ResponseDTO<T> {
 
     getError(): string | undefined {
         return this.error;
+    }
+
+    toJSON() {
+        return {
+            success: this.success,
+            data: this.data,
+            message: this.message,
+            error: this.error
+        };
     }
 }
 
