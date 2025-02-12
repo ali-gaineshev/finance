662b44b79b79e29ddff2825b89f698fdc2753191
Ali Gaineshev <ggaineshev@gmail.com>
2025-02-11 22:13:58 -0800

    Add user object to express request

---
+++ server/src/types/index.d.ts
@@ -0,0 +1,9 @@
+import * as express from "express";
+import { LoginJWTPayload } from "../models/interfaces";
+import { Express } from "express-serve-static-core";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: LoginJWTPayload;
+  }
+}
