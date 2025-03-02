2c326a60205bc38b7ada5a789c7effdd9b73d6fd
Ali Gaineshev <ggaineshev@gmail.com>
2025-03-02 13:43:17 -0800

    AuthProvider updates token on refresh now

--- client/src/context/AuthProvider.tsx
+++ client/src/context/AuthProvider.tsx
@@ -1,8 +1,9 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import {
   AuthContextType,
   defaultAuthContext,
 } from "../types/AuthContextType.ts";
+import useRefreshToken from "../hooks/useRefreshToken.tsx";
 
 export const AuthContext = createContext<AuthContextType>(defaultAuthContext);
 
@@ -13,7 +14,32 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
     token?: string;
     userState?: object;
   } | null>(null);
-  console.log("AUTH IS ", auth);
+  const [loading, setLoading] = useState(true); // loading state. We want to refresh token before the loading finishes
+
+  const refreshToken = useRefreshToken();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { token, userState } = await refreshToken();
+        setAuth({ token, userState });
+      } catch (err) {
+        setAuth(null);
+      } finally {
+        setLoading(false); // Done loading
+      }
+    })();
+  }, []);
+
+  // Use an effect to log state updates
+  useEffect(() => {
+    console.log("AUTH UPDATED IS ", auth);
+  }, [auth]); // Runs whenever auth changes
+
+  if (loading) {
+    return <div>Loading...</div>; // Show loading until token is refreshed
+  }
+
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
       {children}
