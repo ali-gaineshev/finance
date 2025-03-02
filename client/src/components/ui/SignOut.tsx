da6d3fd0e53c48812109e2cd99931ac24b29bdac
Ali Gaineshev <ggaineshev@gmail.com>
2025-03-02 13:45:07 -0800

    refactoring

--- client/src/components/ui/SignOut.tsx
+++ client/src/components/ui/SignOut.tsx
@@ -2,14 +2,14 @@ import { Navigate } from "react-router-dom";
 import { apiClient } from "../../api/ApiClient.ts";
 import { useState } from "react";
 import alert from "./Alert.tsx";
-import BACKEND from "../../api/BACKEND.ts";
+import BACKEND_ENDPOINT from "@shared/types/endpoints.ts";
 
 export const SignOutComponent = () => {
   const [redirect, setRedirect] = useState(false);
 
   const signOutAndRedirect = async () => {
     try {
-      const res = await apiClient.post(BACKEND.LOGOUT);
+      const res = await apiClient.post(BACKEND_ENDPOINT.LOGOUT_FULL_PATH);
       alert(res?.data?.message, "success");
     } catch (e) {
       console.error(e);
